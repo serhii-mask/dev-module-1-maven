e5a9cf055e50bc364e0a99215eb6b44142cbc456
Serhii <serhii.mask@gmail.com>
2023-07-18 21:57:59 +0300

    feat: add person

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.example.person.Person;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Person person = new Person();
+        person.setName("Ivan");
+        person.setAge(32);
+        System.out.println("person = " + person);
     }
 }
---
+++ src/main/java/org/example/person/Person.java
@@ -0,0 +1,9 @@
+package org.example.person;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    String name;
+    int age;
+}
